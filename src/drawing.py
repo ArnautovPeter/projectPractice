714e1dc5ab7d839d92950c209458b18019989e6d
Arnautov <j32arnautov@gmail.com>
2024-05-14 00:19:17 +0300

    ok

--- src/drawing.py
+++ src/drawing.py
@@ -25,3 +25,14 @@ def draw_on_plot(Ox: list[list], Oy: list[list], names: list[str],
     else:
         for x, y in zip(Ox, Oy, names):
             px1.scatter(x, y, label=name)
+
+"Рисование графиков"
+def draw_on_plot(Ox: list[list], Oy: list[list], names: list[str],
+                 px1, xlable, ylable, scatter: bool = False):
+    if not scatter:
+        for x, y, name in zip(Ox, Oy, names):
+            px1.plot(x, y, label=name)
+    else:
+        for x, y in zip(Ox, Oy, names):
+            px1.scatter(x, y, label=name)
+
