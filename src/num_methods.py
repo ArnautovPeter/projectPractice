9c7fa33980cfcdd77624ffdb2b60b167b993d55c
Barahta <66131926+JustBadWeb@users.noreply.github.com>
2024-05-17 19:55:18 +0300

    Поддержка неявного первого порядка в Solver2

--- src/num_methods.py
+++ src/num_methods.py
@@ -1,9 +1,9 @@
 import math
-from typing import Callable
+
+from scipy import optimize
 
 
 class Solver2():
-    # не сработает если есть t, т.е. независимая переменная
     def __init__(self, func: tuple[callable, callable],
                 params: list[float],
                 init_val: tuple[float, float],
@@ -18,22 +18,22 @@ class Solver2():
         self._t_limits = t_limits
         self._params = params
 
-    """Изменение параметров уравнения"""
     def set_params(self, new_params):
+        """Изменение параметров уравнения"""    
         self._params = new_params
 
-    """Изменение диапазона интегрирования"""
     def set_t_limits(self, new_limits):
+        """Изменение диапазона интегрирования"""
         self._t_limits = new_limits
 
     # ЯВНЫЕ МЕТОДЫ
 
-    """Явный метод Эйлера 1-го порядка"""
     def explicit1(self, T, xvn, yvn, t, _):
+        """Явный метод Эйлера 1-го порядка"""
         return self._runge_kutta_exp(T, xvn, yvn, [], [1], [], t)
   
-    """Явный метод Рунге-Кутты 4-го порядка"""
     def explicit4(self, T, xvn, yvn, t, _):
+        """Явный метод Рунге-Кутты 4-го порядка"""
         a = [[1/2],
              [0, 1/2],
              [0, 0, 1]]
@@ -41,8 +41,8 @@ class Solver2():
         c = [1/2, 1/2, 1]
         return self._runge_kutta_exp(T, xvn, yvn, a, b, c, t)
   
-    """Явный метод Рунге-Кутты 5-го порядка"""
     def explicit5(self, T, xvn, yvn, t, _):
+        """Явный метод Рунге-Кутты 5-го порядка"""
         a = [[1/4],
              [3/32, 9/32],
              [1932/2197, -7200/2197, 7296/2197],
@@ -74,21 +74,31 @@ class Solver2():
         return (xn, yn)
     
     # НЯВНЫЕ МЕТОДЫ
+
+    def _guess(self, T, xvn, yvn, t):
+        # TODO: maybe xvn is better or not, needs to be checked
+        return [xvn + T * self._fx(T, xvn, yvn, t),
+                yvn + T * self._fy(T, xvn, yvn, t)]
     
-    def implicit1(self, T, xvn, yvn, t):
+    def implicit1(self, T, xvn, yvn, t, jac):
         def fk(x):
-            return [T * fx(t, x[0], x[1], *self._params) - x[0] + xvn,
-                    T * fy(t, x[0], x[1], *self._params) - x[1] + yvn]
+            return [T * self._fx(t, x[0], x[1], *self._params) - x[0] + xvn,
+                    T * self._fy(t, x[0], x[1], *self._params) - x[1] + yvn]
+        
+        # print(jac(T, xvn, yvn, t, *self._params))
+        sol = optimize.root(fun=fk, x0=self._guess(T, xvn, yvn, t),
+                        jac=jac(T, xvn, yvn, t, *self._params), method='hybr')
+        return (sol.x[0], sol.x[1])
 
     # ВСПОМОГАТЕЛЬНЫЕ МЕТОДЫ
 
-    # новый шаг
     def get_p(self, array):
+        """ новый шаг """
         s = sum([i ** 2 for i in array])
         return math.sqrt(s)
 
-    # T0 - максимальный возможный шаг
     def new_T(self, last_T, T0, array, array_prev, L):
+        """ T0 - максимальный возможный шаг """
         p = self.get_p(array)
         F = [i / p for i in array]
         p_prev = self.get_p(array_prev)
@@ -98,36 +108,49 @@ class Solver2():
         new_T = T0 / (1 + math.sqrt(L) * XX ** (1/4))
         return new_T
 
-    """
-    Выполнение метода
-    method - метод, которым надо решить
-    T0 - максимальный шаг интегрирования, если динам., иначе просто шаг
-    dynamic_step - использовать ли динамический шаг
-    
-    -----
-    
-    return ([независимая переменная], [вычисленные значения для ф-ий],
-            [вычисленные для дифф], [названия методов])
-    """
-    def do_method(self, method, T0, dynamic_step: bool=True, jac_impl: list[callable]) -> tuple[list[float],
+    def do_method(self, method, T0, jac_impl: callable=None, dynamic_step: bool=False) -> tuple[list[float],
                                                                                                 list[tuple[float, float]],
                                                                                                 list[tuple[float, float]],
                                                                                                 str]:
+        """
+        Выполнение численного метода\n
+        method - метод, которым надо решить\n
+        T0 - максимальный шаг интегрирования, если динам., иначе просто шаг\n
+        якобиан системы уравнений, которая является решением для неявной схемы, передается в виде
+        jac(T, xvn, yvn, t, params) и содержит внутри функцию, которую нужно передать optimize.root\n
+        dynamic_step - использовать ли динамический шаг
+
+        -----
+        
+        return ([независимая переменная], {вычисленные значения для ф-ий}, [названия методов])
+        """
+        
         tl = [self._t_limits[0]]
         T = T0
 
         # список переменных в виде (x, y)
         array: list[tuple] = [self._init_val]
         array_T = [T]
-        array_dif = [(self._fx(tl[-1], array[-1][0], array[-1][1], *self._params),
-                        self._fy(tl[-1], array[-1][0], array[-1][1], *self._params))]
         while tl[-1] <= self._t_limits[1]:
             array.append(method(T, array[-1][0], array[-1][1], tl[-1], jac_impl))
-            # array_dif.append((self._fx(tl[-1], array[-1][0], array[-1][1], *self._params),
-            #         self._fy(tl[-1], array[-1][0], array[-1][1], *self._params)))
             if dynamic_step:
                 T = self.new_T(array_T[-1], T0, array[-1], array[-2],
                                 self._t_limits[1] - self._t_limits[0])
             array_T.append(T)
             tl.append(tl[-1] + T)
-        return (tl, array, array_dif, method.__name__)
+        return (tl, array, method.__name__)
+    
+    @staticmethod
+    def get_x(array):
+        """Получение первой координаты из кортежа от do_method"""
+        return [i[0] for i in array[1]]
+
+    @staticmethod
+    def get_y(array):
+        """Получение второй координаты из кортежа от do_method"""
+        return [i[1] for i in array[1]]
+    
+    def get_diffs(self):
+        array_dif = [(self._fx(tl[-1], array[-1][0], array[-1][1], *self._params),
+                        self._fy(tl[-1], array[-1][0], array[-1][1], *self._params))]
+    
