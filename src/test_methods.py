1442d8d0a925a2231bf6ad0dc8fcd12ca67f7dab
Arnautov <j32arnautov@gmail.com>
2024-05-20 19:56:44 +0300

    фикс всякого

--- src/test_methods.py
+++ src/test_methods.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from statistics import mean
-
+from drawing import draw
 
 class Test2():
     def __init__(self, f: tuple[callable, callable]):
@@ -25,13 +25,18 @@ class Test2():
         max_error = []
         while TL[-1] <= T_limits[1]:
             results = [sol.do_method(method, TL[-1], dynamic_step=False) for method in methods]
+            # args = np.linspace(T_limits[0], T_limits[1], 500)
+            # # args = np.linspace(t_start, t_finish, int((t_finish - t_start) / T))
+            # x_true = list([self._fx(t) for t in args])
+            # draw([[i for i in results[0][0]], args], [[i[0] for i in results[0][1]], x_true], ['fake', 'real'], ['fake', 'real'])
+            # return
             max_error_step = [0] * len(methods)
             tl = results[0][0]  # т.к. для всех одинаковое количество шагов
             for i in range(len(tl)):
                 real_x = self._fx(tl[i])
                 real_y = self._fy(tl[i])
                 for j in range(len(methods)):
-                    max_error_step[j] = max(max_error_step[j], abs(results[j][1][i][0] - real_x), abs(results[j][1][i][0] - real_y))
+                    max_error_step[j] = max(max_error_step[j], abs(results[j][1][i][0] - real_x), abs(results[j][1][i][1] - real_y))
             max_error.append(max_error_step)
             TL.append(TL[-1] + step)  # один лишний будет
 
@@ -40,18 +45,29 @@ class Test2():
         TL = [(math.log(t) - crd_move_t) for t in TL[:-1]]
         for i in range(len(methods)):
             crd_move_f = math.log(max_error[0][i])
-            max_error_curr = [row[i] - crd_move_f for row in max_error]
+            # print(crd_move_f, math.log(max_error[0][i]))
+            max_error_curr = [math.log(row[i]) - crd_move_f for row in max_error]
 
-            fig = plt.figure(figsize=(8,8))
+            ls = 15
+            fs = 6
+            fonts = 15
+            fig = plt.figure(figsize=(fs,fs))
             px1 = fig.add_subplot(111)
-            px1.plot(TL, max_error_curr, label=methods[i].__name__ + f" {mean([(max_error_curr[i]/TL[i]) for i in range(len(TL))])}")
+            print(TL)
+            px1.plot(TL, max_error_curr, label=methods[i].__name__ + f" {mean([(max_error_curr[i]/TL[i]) for i in range(1, len(TL))])}")
 
             # непонятный код))
-            px1.set_xticks(np.arange(0, math.ceil(math.log(TL[-1]) - crd_move_t) + 1, step=1))
-            px1.set_yticks(np.arange(0, math.ceil(math.log(TL[-1]) - crd_move_f) + 1, step=1))
-            px1.set_xlabel("ln(T)")
-            px1.set_ylabel("ln(E(T))")
-            px1.legend()
+            px1.set_xticks(np.arange(0, math.ceil(TL[-1]) + 1, step=1))
+            try:
+                px1.set_yticks(np.arange(0, math.ceil(max_error_curr[-1]) + 1, step=1))
+            except:
+                px1.set_yticks(np.arange(0, math.ceil(TL[-1]) * 9, step=5))
+            # px1.set_xlabel("ln(T)")
+            # px1.set_ylabel("ln(E(T))")
+            px1.set_xlabel("ln(T)", fontsize = fonts)
+            px1.set_ylabel("ln(E(T))", fontsize = fonts)
+            px1.legend(fontsize=fonts-3)
+            px1.tick_params(axis="both", which="major", labelsize=ls)
             fig.show()
                 
             
